42143d3c6c3ee86e56614fc66371131fa9c31d4c
Owen Schlimgen <oschlimgen@gmail.com>
2026-04-08 15:18:42 -0700

    Started magnet simulation code

---
+++ MagnetSimulation.py
@@ -0,0 +1,39 @@
+# 1. Randomly generate a matrix of spins
+# 2. Choose a random atom and consider flipping the orientation of its spin
+# 3. Calculate the probablility of accepting the change based on the formula given
+#     - For each neighboring atom calculate dE then add them to get total dE
+# 4. Randomly accept or reject the flipping of the atom based on calculated probability
+#     - Generate a random number and compare to the probability
+#     - Actually flip if accepted
+# 5. Go back to 2 and repeat some fixed finite number of times
+#     - Could consider stopping based on reaching some "equilibrium probability" but may be difficult to be consistent across a wide range of temperatures.
+# 6. Calculate magnetization
+#     - Sum all spin values in the lattice then take absolute value
+#     - "Normalize" by dividing by number of atoms in the lattice
+# 7. Go back to 1 and repeat for different temperatures
+#     - Going from 0.1 --> 1000 for T
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+SIZE = 30
+
+
+def createSpinMatrix():
+  return (2 * np.random.randint(0,2, size=(SIZE, SIZE))) - 1
+
+def chooseRandomAtom():
+  return (np.random.randint(0, SIZE), np.random.randint(0, SIZE))
+
+def calculateEnergyChange(lattice, position):
+  energyChange = 0
+  curOrientation = lattice[position[0]][position[1]]
+  for direction in [(1,0),(0,1),(-1,0),(0,-1)]:
+    adjacentAtom = np.add(curOrientation, direction)
+    if adjacentAtom[0] < 0 or adjacentAtom[1] < 0 or \
+        adjacentAtom[0] >= SIZE or adjacentAtom[1] >= SIZE:
+      continue
+    adjacentOrientation = lattice[adjacentAtom[0]][adjacentAtom[1]]
+    energyChange += (2 * curOrientation * adjacentOrientation)
